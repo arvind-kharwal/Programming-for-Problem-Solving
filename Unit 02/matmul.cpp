4c7315e67a58abaa5f4fd102a585cbbe23ca5bb3
Arvind Kharwal <karvind629@gmail.com>
2023-10-19 11:09:50 +0530

    19 October 2023

---
+++ Unit 02/matmul.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3],s[3][3],i,j,k;
+    printf("\nEnter the elements in Matrix a: ");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("\nThe Entered elements in Matrix a: \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nEnter the elements in Matrix b: ");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("\nThe Entered elements in Matrix b: \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            s[i][j] = 0;
+            for(k=0;k<3;k++)
+            {
+                s[i][j] = s[i][j]+a[i][k]*b[k][j];
+            }
+        }
+    }
+    printf("\nThe elements in Matrix s: \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",s[i][j]);
+        }
+        printf("\n");
+    }
+
+return(0);
+}
