8f1fa28cd36298199ce91b9d2f4c314fe954655b
Arvind Kharwal <karvind629@gmail.com>
2023-11-01 12:47:17 +0530

    1 November 2023

---
+++ Unit 03/Function/swap.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void swap(int,int);
+int main()
+{
+    int a,b;
+    printf("\nEnter the two numbers: ");
+    scanf("%d%d",&a,&b);
+    printf("\nThe numbers before swapping are: %d%d",a,b);
+    swap(a,b);
+    return(0);
+}
+void swap(int x,int y)
+{
+    int t;
+    t = x;
+    x = y;
+    y = t;
+    printf("\nThe numbers after swapping are: %d%d",x,y);
+}
