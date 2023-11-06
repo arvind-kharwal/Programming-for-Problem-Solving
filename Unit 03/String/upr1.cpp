e37dc7d687665d74e723b60d9a3a6285dbfab1e7
Arvind Kharwal <karvind629@gmail.com>
2023-11-06 16:07:13 +0530

    6 November 2023

--- Unit 03/String/upr1.cpp
+++ Unit 03/String/upr1.cpp
@@ -3,11 +3,17 @@
 int main()
 {
     char s[50];
+    int i=0;
     printf("Enter the string in Lower Case: ");
     gets(s);
     printf("\nThe String S in Lower Case: ");
     puts(s);
-    strupr(s); // 
+    while(s[i]!='\0')
+    {
+        if((s[i]>=65&&s[i]<=90)||(s[i]>=97&&s[i]<=126))
+            s[i] = s[i]-32;
+        i++;
+    }
     printf("\nThe String in Uppercase: ");
     puts(s);
 }
