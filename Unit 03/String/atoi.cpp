aa3785683ee2efb6b9b48eac65f886116372bed3
Arvind Kharwal <karvind629@gmail.com>
2023-11-08 09:30:08 +0530

    8 November 2023

---
+++ Unit 03/String/atoi.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char s1[] = "1234";
+    char s2[] = "21Hello";
+    int m,n;
+    n = atoi(s1);
+    m = atoi(s2);
+    printf("%d",n);
+    printf("\n%d",m);
+    
+}
