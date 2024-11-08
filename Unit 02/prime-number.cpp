57939d93fe2bcdc50898cba61995ae20afe51b1a
Arvind Kharwal <karvind629@gmail.com>
2024-11-08 09:54:09 +0530

    8 November 2024

---
+++ Unit 02/prime-number.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,count = 0;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    if(n==0||n==1)
+        count =1;
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+            break;
+        }
+    }
+    if(count==0&&n!=1)
+        printf("%d is prime number",n);
+    else
+        printf("%d is not prime",n);
+}
