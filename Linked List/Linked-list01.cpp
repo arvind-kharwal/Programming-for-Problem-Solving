8ccf94074e40ccd4278245738907bdf0d09561eb
Arvind Kharwal <karvind629@gmail.com>
2025-01-09 09:06:34 +0530

    09 January 2025

---
+++ Linked List/Linked-list01.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *link;
+};
+void display(struct node *);
+int main()
+{
+    struct node *head;
+    head=(struct node *)malloc(sizeof(struct node));
+    head->data=45;
+    head->link=NULL;
+
+    struct node *current;
+    current=(struct node *)malloc(sizeof(struct node));
+    current->data=98;
+    current->link=NULL;
+    head->link=current;
+
+    current=(struct node *)malloc(sizeof(struct node));
+    current->data=3;
+    current->link=NULL;
+    head->link->link=current;
+    display(head);
+    return 0;
+}
+void display(struct node *head)
+{
+    struct node *ptr;
+    ptr = head;
+    while(ptr!=NULL)
+    {
+        printf("%d\n",ptr->data);
+        ptr=ptr->link;
+    }
+}
